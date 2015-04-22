2d0d1fdd74118d5ee1408f5eb00e1d8335fdab1d
Varghese Thomas E <varghese.thomas@redpanthers.co>
2015-04-22 20:49:56 +0530

    Added comments for screen resolution

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ if (urlArray.length <= 0) {
     .exit(1);
 }
 var viewportSizes = [
-    [320,480],
-    [320,533],
-    [320,568],
-    [360,640],
-    [375,667],
-    [384,640],
-    [400,640],
-    [424,753],
-    [480,854],
+    [320,480], /* Iphone 4 */
+    [320,533], /* HTC Desire Nokia Lumia*/
+    [320,568], /* Iphone 5 */
+    [360,640], /* Blackberry z30, Nexus 5, HTC One X, Sony Xperia X*/
+    [375,667], /* Iphone 6 */
+    [384,640], /* Blackberry z10, Nexus 4,  */
+    [400,640], /* Samsung Note */
+    [424,753], /* LG Optimus LTE */
+    [480,854], /* Sony Xperia U */
     [960,720],
-    [966,604],
-    [1280,720],
-    [1280,800],
-    [1920,1080]
+    [966,604], /* Nexus 7 */
+    [1024,768],
+    [1280,720], /* HD */
+    [1280,800], /* Nexus 10, Samsung Galaxy Tab*/
+    [1920,1080] /* Full HD */
 ];
 casper.start().eachThen(urlArray, function(response) {
 
@@ -33,7 +34,7 @@ casper.start().eachThen(urlArray, function(response) {
 			this.viewport(viewportSize[0], viewportSize[1]);
 		});
 		this.thenOpen(response.data, function() {
-      this.wait(30000);
+      this.wait(40000);
     });
     this.then(function(){
     	var FPfilename = saveDir + '/fullpage-' + viewportSize[0] + "x" + viewportSize[1] + ".png";
